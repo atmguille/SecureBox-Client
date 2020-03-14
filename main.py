4a6ead55620bb8eccea048117614a431b0957745
Daniel Gallo Fernández <danielsantoangel@gmail.com>
2020-03-14 13:27:42 +0100

    Add file functions

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def user_register(username: str, email: str, public_key: str) -> None:
     parsed_response = json.loads(response.text)
 
     if response.status_code != 200:
-        raise known_exceptions[parsed_response["error_code"]]
+        raise api_exceptions[parsed_response["error_code"]]
 
 
 def user_search(query: str) -> list:
@@ -31,7 +31,7 @@ def user_search(query: str) -> list:
     parsed_response = json.loads(response.text)
 
     if response.status_code != 200:
-        raise known_exceptions[parsed_response["error_code"]]
+        raise api_exceptions[parsed_response["error_code"]]
 
     return parsed_response
 
@@ -44,7 +44,7 @@ def user_get_public_key(user_id: str) -> str:
     parsed_response = json.loads(response.text)
 
     if response.status_code != 200:
-        raise known_exceptions[parsed_response["error_code"]]
+        raise api_exceptions[parsed_response["error_code"]]
 
     return parsed_response["publicKey"]
 
@@ -57,6 +57,59 @@ def user_delete(user_id: str) -> str:
     parsed_response = json.loads(response.text)
 
     if response.status_code != 200:
-        raise known_exceptions[parsed_response["error_code"]]
+        raise api_exceptions[parsed_response["error_code"]]
 
     return parsed_response["userID"]
+
+
+def file_list() -> list:
+    url = base_url + "/files/list"
+
+    response = requests.get(url, headers=header)
+    parsed_response = json.loads(response.text)
+
+    if response.status_code != 200:
+        raise api_exceptions[parsed_response["error_code"]]
+
+    return parsed_response["files_list"]
+
+
+def file_upload(filename: str) -> str:
+    url = base_url + "/files/upload"
+    f = open(filename, "rb")
+    body = {"ufile": f}
+
+    # Note that this is the only function of the API which receives an ordinary POST form instead of a JSON one
+    response = requests.post(url, headers=header, files=body)
+    parsed_response = json.loads(response.text)
+
+    if response.status_code != 200:
+        raise api_exceptions[parsed_response["error_code"]]
+
+    return parsed_response["file_id"]
+
+
+def file_download(file_id: str) -> str:
+    url = base_url + "/files/download"
+    body = {"file_id": file_id}
+
+    response = requests.post(url, headers=header, json=body)
+
+    if response.status_code != 200:
+        parsed_response = json.loads(response.text)
+        raise api_exceptions(parsed_response["error_code"])
+
+    return response.text
+
+
+def file_delete(file_id: str) -> str:
+    url = base_url + "/files/delete"
+    body = {"file_id": file_id}
+
+    response = requests.post(url, headers=header, json=body)
+    parsed_response = json.loads(response.text)
+
+    if response.status_code != 200:
+        raise api_exceptions[parsed_response["error_code"]]
+
+    return parsed_response["file_id"]
