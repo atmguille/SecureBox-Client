4492e3b22dfcbcef790f2a16901e1b1b70f8a82b
Guillermo Garcia <ggcobo806@gmail.com>
2020-03-19 21:02:23 +0100

    Do not encrypt IV with RSA. Updated strategy when decrypting relating to this

--- crypto.py
+++ crypto.py
@@ -1,86 +1,54 @@
 from Crypto.PublicKey import RSA
 from Crypto.Hash import SHA256
-from Crypto.Signature import pss
+from Crypto.PublicKey.RSA import RsaKey
+from Crypto.Signature import pkcs1_15
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
-from log import logger
-from exceptions import *
+from api.exceptions import *
 
-log = logger.get_logger()
-
-
-def rsa_generate_key(nbits=2048):
-    key = RSA.generate(nbits)
-    with open('keys/rsa_private_key.pem', 'wb') as f:
-        f.write(key.export_key('PEM'))
+IV_LEN = 16
 
 
-def rsa_get_private_key():
-    with open('keys/rsa_private_key.pem', 'r') as f:
-        return RSA.import_key(f.read())
+def rsa_generate_key(nbits=2048) -> RsaKey:
+    return RSA.generate(nbits)
 
 
-def rsa_get_public_key():
-    return rsa_get_private_key().publickey()  # TODO: mejor así o guardarla en un fichero aparte y leerla de ahí?
-
-
-def sign_message(message):
-    try:
-        rsa_private_key = rsa_get_private_key()
-    except FileNotFoundError:
-        log.warning("No private key found")
-        raise PrivateKeyNotFound
-    h = SHA256.new(message)
-    return pss.new(rsa_private_key).sign(h)
+def sign_message(message, key: RsaKey):
+    return pkcs1_15.new(key).sign(SHA256.new(message))
 
 
 def verify_signature(message, sender_public_key):
     signature = message[:sender_public_key.size_in_bytes()]  # Assume encryption has been done with same key size
     original_message = message[sender_public_key.size_in_bytes():]
     h = SHA256.new(original_message)
-    verifier = pss.new(sender_public_key)
+    verifier = pkcs1_15.new(sender_public_key)
     try:
         verifier.verify(h, signature)
+        return original_message
     except (ValueError, TypeError):
         raise SignatureNotAuthentic
 
 
 def encrypt_message(message, receiver_public_key, nbits=256) -> bytes:
-    aes_key = get_random_bytes(nbits//8)
+    aes_key = get_random_bytes(nbits // 8)
     cipher_aes = AES.new(aes_key, AES.MODE_CBC)
-    return encrypt_aes_key(cipher_aes.iv + aes_key, receiver_public_key) + cipher_aes.encrypt(pad(message, AES.block_size))
+    return cipher_aes.iv + _encrypt_aes_key(aes_key, receiver_public_key) + cipher_aes.encrypt(
+        pad(message, AES.block_size))
 
 
-def encrypt_aes_key(aes_key, receiver_public_key) -> bytes:
+def _encrypt_aes_key(aes_key, receiver_public_key) -> bytes:
     cipher_rsa = PKCS1_OAEP.new(receiver_public_key)
     return cipher_rsa.encrypt(aes_key)
 
 
-def decrypt_message(message):
-    try:
-        rsa_private_key = rsa_get_private_key()
-    except FileNotFoundError:
-        log.warning("No private key found")
-        raise PrivateKeyNotFound
+def decrypt_message(message, key: RsaKey):
+    iv = message[:IV_LEN]
+    enc_aes_key = message[IV_LEN:IV_LEN + key.size_in_bytes()]  # Assume encryption has been done with same key size
+    enc_message = message[IV_LEN + key.size_in_bytes():]
 
-    enc_aes_key = message[:rsa_private_key.size_in_bytes()]  # Assume encryption has been done with same key size
-    enc_message = message[rsa_private_key.size_in_bytes():]
-
-    cipher_rsa = PKCS1_OAEP.new(rsa_private_key)
-    aes_key_and_iv = cipher_rsa.decrypt(enc_aes_key)
-    iv = aes_key_and_iv[:16]  # IV is always 16 bytes long when encrypting with AES
-    aes_key = aes_key_and_iv[16:]
+    cipher_rsa = PKCS1_OAEP.new(key)
+    aes_key = cipher_rsa.decrypt(enc_aes_key)
 
     cipher_aes = AES.new(aes_key, AES.MODE_CBC, iv)
     return unpad(cipher_aes.decrypt(enc_message), AES.block_size)
-
-
-data = b'message secret'
-rsa_generate_key()
-signature = sign_message(data)
-enc_message = encrypt_message(signature + data, rsa_get_public_key())
-print(enc_message)
-orig_message = decrypt_message(enc_message)
-print(orig_message)
-verify_signature(orig_message, rsa_get_public_key())
