9674c4a8380c7ba2ee964257392fe9aa9e0f7d66
Guillermo Garcia <ggcobo806@gmail.com>
2020-03-19 22:18:50 +0100

    Add comment about crypto exception

--- crypto/exceptions.py
+++ crypto/exceptions.py
@@ -1,4 +1,4 @@
-class SignatureNotAuthentic(Exception):
+class SignatureNotAuthentic(Exception):  # TODO: este fichero solo para una excepcion? Es por seguir el modelo de api, pero...
     def __init__(self):
         message = "The signature is not authentic"
         super().__init__(message)
