16afeeb18e718eef7f2cc0457dbaadc433930340
Daniel Gallo Fernández <danielsantoangel@gmail.com>
2020-03-20 17:57:10 +0100

    Rename crypto to cryptography to avoid name collisions

--- cryptography/cryptography.py
+++ cryptography/cryptography.py
@@ -5,7 +5,7 @@ from Crypto.Signature import pkcs1_15
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
-from crypto.exceptions import *
+from cryptography.exceptions import *
 
 IV_LEN = 16
 
